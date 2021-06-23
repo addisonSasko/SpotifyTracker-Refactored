35dba8044a7cae6378112bc86456a1c74a960a10
Addison Sasko <addison.sasko1@gmail.com>
2021-06-23 13:41:54 -0600

    Added basic API service layer

---
+++ src/users/services/users.service.ts
@@ -0,0 +1,35 @@
+import usersDao from "../daos/users.dao";
+import { CRUD } from "../../common/interfaces/crud.interface";
+import { CreateUserDto } from "../../dto/create.user.dto";
+import { PutUserDto } from "../../dto/put.user.dto";
+import { PatchUserDto } from "../../dto/patch.user.dto";
+
+class UsersService implements CRUD {
+
+    async create(resource: CreateUserDto) {
+        return usersDao.addUser(resource);
+    }
+
+    async list(limit: number, page: number) {
+        return usersDao.getUsers();
+    }
+        
+    async putById(id: string, resource: any) {
+        return usersDao.putUserById(id, resource);
+    };
+    
+    async readById(id: string){ 
+        return usersDao.getUserById(id);
+    };
+
+    async deleteById(resource: string) {
+        return usersDao.removeUserById(resource);
+    }
+    
+    async patchById(id: string, resource: any) {
+        return usersDao.patchUserById(id, resource);
+    };
+
+}
+
+export default new UsersService();
